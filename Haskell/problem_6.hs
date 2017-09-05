2dd8b08278fff6c4899680210242a28a26c0d2be
Hsekiu <kpetr7@gmail.com>
2017-09-04 20:28:32 -0700

    Updated badge and problem 6 code.

--- Haskell/problem_6.hs
+++ Haskell/problem_6.hs
@@ -3,4 +3,4 @@ sumsquare n = fromIntegral (n*(n+1)*(2*n+1)) `div` 6
 
 squaresum n = (sum [1..n])^2
 
-main = print ((sumsquare 100) - (squaresum 100))
+main = print (abs(((sumsquare 100) - (squaresum 100))))
